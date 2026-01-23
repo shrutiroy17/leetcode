5ecae133eae35134fe063600af1f856ee11f6a02
Shruti Roy <165298428+shrutiroy17@users.noreply.github.com>
2026-01-23 12:22:07 +0530

    Time: 0 ms (100%), Space: 13.9 MB (40.24%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& nums) {
-        unordered_map<int, int> mpp;
-        for (int i = 0; i < nums.size(); i++) {
-            int target1 = 2 * nums[i];
-            int target2 = nums[i] / 2;
-            if (mpp.find(target1) != mpp.end() ||
-                (nums[i] % 2 == 0 && mpp.find(target2) != mpp.end())) {
-                return true;
-            }
-            mpp[nums[i]]++;
+        unordered_set<int> seen;
+        for(auto x : nums){
+            if(seen.count(2*x) || (x%2==0 && seen.count(x/2)))
+            return true;
+            seen.insert(x);
         }
-
         return false;
     }
 };
