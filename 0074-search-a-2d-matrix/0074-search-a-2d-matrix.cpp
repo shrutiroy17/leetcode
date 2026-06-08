7704da1511405ee1a35e0a41b36e5124afc7d5b4
Shruti Roy <165298428+shrutiroy17@users.noreply.github.com>
2026-06-08 11:51:18 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.6%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,15 +3,33 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int low = 0 , high = n*m-1;
+        //guess the row value and once detected -> apply BS on row
+        int low = 0 , high = n-1, row = -1;
         while(low<=high){
             int guess = (low+high)/2;
-            int row = guess/m;
-            int col = guess%m;
-            if(matrix[row][col]==target) return true;
-            else if (matrix[row][col]>target) high = guess-1;
-            else low = guess+1;
+            if(matrix[guess][m-1]>=target){
+                row = guess;
+                high = guess-1;//since we need to find the 1st row in which target occurs 
+            }
+            else{
+                low = guess+1;
+            }
+        }
+        if(row==-1) return false;
+        //search space now low = row , high = m-1;
+        int left = 0 , right = m-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            int val = matrix[row][mid];
+            if(val==target) return true;
+            else if(val>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
         }
         return false;
     }
+    
 };
