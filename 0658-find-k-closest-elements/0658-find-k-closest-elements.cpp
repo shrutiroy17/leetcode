ceaac06d511900ca9cf90c26569484c6103089f2
Shruti Roy <165298428+shrutiroy17@users.noreply.github.com>
2026-06-12 11:47:10 +0530

    Time: 23 ms (29.59%), Space: 38.5 MB (33.92%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    struct cmp{
+        bool operator() (const pair<int,int>& a, const pair<int,int>& b){
+            return a.first<b.first; // map heap automated
+        }
+    };
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
+        //heap holds distance,point
+        for(int p : arr){
+            int dist = abs(x-p);
+            if(pq.size()<k){
+                pq.push({dist,p});
+            }
+            else if(dist<pq.top().first){
+                pq.pop();
+                pq.push({dist,p});
+            }
+        }
+        vector<int>res;
+        while(!pq.empty()){
+            res.push_back(pq.top().second); // since it is a max heap so res will be in descending order
+            pq.pop();
+        }
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
