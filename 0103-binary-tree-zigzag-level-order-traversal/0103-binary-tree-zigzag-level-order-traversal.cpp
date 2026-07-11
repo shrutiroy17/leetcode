1148291a262dc2ffd59038d687eed040d1283752
Shruti Roy <165298428+shrutiroy17@users.noreply.github.com>
2026-07-11 20:40:00 +0530

    Time: 0 ms (100%), Space: 14.9 MB (94.79%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -6,37 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(!root) return {};
         vector<vector<int>> res;
-        queue<TreeNode*> q;
-        if (root == NULL) {
-            return {};
-        }
+        queue<TreeNode* >q;
         q.push(root);
-        bool rev = false;
-        while (!q.empty()) {
-            int lvlSize = q.size();
-            vector<int> temp;
-            while (lvlSize--) {
+        bool leftToRight = true;
+        while(!q.empty()){
+            int levelSize = q.size();
+            vector<int>temp(levelSize);
+            int front = 0;
+            int last = levelSize-1;
+            while(levelSize--){
                 TreeNode* t = q.front();
                 q.pop();
-                temp.push_back(t->val);
-                if(t->left!=NULL) q.push(t->left);
-                if(t->right!=NULL) q.push(t->right);
+                if(leftToRight){
+                    temp[front] = t->val;
+                    front++;
+                }
+                else{
+                    temp[last] = t->val;
+                    last--;
+                }
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
             }
-            if(rev){
-                reverse(temp.begin(),temp.end());
-                res.push_back(temp);
-            }
-            else
+            leftToRight = !leftToRight;
             res.push_back(temp);
-            rev = !rev;
         }
         return res;
     }
