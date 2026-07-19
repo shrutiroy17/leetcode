82760ad61eb96a41e687358c181cafa8a931f906
Shruti Roy <165298428+shrutiroy17@users.noreply.github.com>
2026-07-19 15:56:03 +0530

    Time: 0 ms (100%), Space: 37.1 MB (82.28%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,50 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    stack<TreeNode*> asc;
-    stack<TreeNode*> desc;
-    TreeNode* getSmallest() {
-        if (asc.empty()) return NULL;
-        TreeNode* small = asc.top();
-        asc.pop();
-        TreeNode* rightChild = small->right;
-        while (rightChild) {
-            asc.push(rightChild);
-            rightChild = rightChild->left;
-        }
-        return small;
-    }
-    TreeNode* getLargest() {
-        if (desc.empty()) return NULL;
-        TreeNode* big = desc.top();
-        desc.pop();
-        TreeNode* leftChild = big->left;
-        while (leftChild) {
-            desc.push(leftChild);
-            leftChild = leftChild->right;
-        }
-        return big;
+    vector<int>treeList;
+    void inorder(TreeNode* root){
+        if(root==NULL) return;
+        inorder(root->left);
+        treeList.push_back(root->val);
+        inorder(root->right);
+        return;
     }
     bool findTarget(TreeNode* root, int k) {
-        if (root == NULL) return false;
-        TreeNode* t = root;
-        while (t) {
-            asc.push(t);
-            t = t->left;
-        }
-        t = root;
-        while (t) {
-            desc.push(t);
-            t = t->right;
-        }
-        TreeNode* i = getSmallest();
-        TreeNode* j = getLargest();
-        while (i && j && i != j && i->val < j->val) {
-            int sum = i->val + j->val;
-            if (sum == k) return true;
-            else if (sum > k)
-                j = getLargest();
-            else
-                i = getSmallest();
+        //find inorder traversal -> resultanat array is sorted list
+        inorder(root);
+        int i = 0 , j = treeList.size()-1;
+        while(i<j){
+            int sum = treeList[i]+treeList[j];
+            if(sum==k){
+                return true;
+            }
+            else if(sum>k) j--;
+            else i++;
         }
         return false;
     }
