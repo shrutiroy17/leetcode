ebc5e3bc3c0252712f5a9afbe18264b1576ee6f7
Shruti Roy <165298428+shrutiroy17@users.noreply.github.com>
2026-07-25 23:59:17 +0530

    Time: 1 ms (92.25%), Space: 12.9 MB (75.77%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -4,34 +4,30 @@ public:
         if(i<0 || i>=n || j<0 || j>=m) return false;
         return true;
     }
-    int x[4] = {0,0,-1,1};
-    int y[4] = {-1,1,0,0};
     int swimInWater(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> res(n, vector<int>(m, INT_MAX));
+        int x[4] = {-1,1,0,0};
+        int y[4] = {0,0,-1,1};
         res[0][0] = grid[0][0];
-        
-priority_queue<
-    pair<int, pair<int, int>>, 
-    vector<pair<int, pair<int, int>>>, 
-    greater<pair<int, pair<int, int>>>
-> pq;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
         pq.push({grid[0][0],{0,0}});
         while(!pq.empty()){
             pair<int,pair<int,int>>p = pq.top();
             pq.pop();
-            int wt = p.first;
-            int i = p.second.first;
-            int j = p.second.second;
+            int d = p.first;
+            int r = p.second.first;
+            int c = p.second.second;
+            if(d>res[r][c]) continue;
             for(int k=0;k<4;k++){
-                int r = i+x[k];
-                int c = j+y[k];
-                if(!isValid(r,c,n,m)) continue;
-                int newAns = max(wt,grid[r][c]);
-                if(newAns<res[r][c]){
-                    res[r][c] = newAns;
-                    pq.push({newAns,{r,c}});
+                int row = r + x[k];
+                int col = c + y[k];
+                if(!isValid(row,col,n,m)) continue;
+                int nWeight = max(d,grid[row][col]);
+                if(nWeight<res[row][col]){
+                    res[row][col] = nWeight;
+                    pq.push({nWeight,{row,col}});
                 }
             }
         }
